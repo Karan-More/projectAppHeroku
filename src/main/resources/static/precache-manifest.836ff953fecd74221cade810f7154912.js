edbd3d6aa3ba07b906a79a6f5c5ed719d3165bec
Karan More <karanmore2393@gmail.com>
2019-11-22 05:20:59 -0500

    second-commit-react

---
+++ src/main/resources/static/precache-manifest.836ff953fecd74221cade810f7154912.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1df21ff71cf2b9caed3a1b895233482f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c06310da36daa27622ff",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "46928278267c9a681b1c",
+    "url": "/static/css/main.5ad7ffb2.chunk.css"
+  },
+  {
+    "revision": "c06310da36daa27622ff",
+    "url": "/static/js/2.22a69087.chunk.js"
+  },
+  {
+    "revision": "46928278267c9a681b1c",
+    "url": "/static/js/main.123d37e4.chunk.js"
+  },
+  {
+    "revision": "2af10ffb916494ef38c1",
+    "url": "/static/js/runtime-main.93166ef5.js"
+  }
+]);
